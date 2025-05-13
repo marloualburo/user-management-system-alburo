a057e8c608c6dde45a33cb322ac7f10196b08084
unknown <YY202@ccs.local>
2025-05-13 16:55:35 +0800

    1st

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://user-management-sancija.onrender.com' 
+    apiUrl: 'https://user-management-system-alburo.onrender.com' 
   };
